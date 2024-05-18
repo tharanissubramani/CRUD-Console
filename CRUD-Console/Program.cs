e74d3516479b4202e10072cf1662be22ab05fcca
tharani <tharanis.subramani@gmail.com>
2024-05-18 10:03:15 +0530

    Add project files.

---
+++ CRUD-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
